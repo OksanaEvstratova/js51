d676272184e810ecc507dbfe9cf223b7e8f7fa85
OksanaEvstratova <v.e.oksana@gmail.com>
2025-01-29 13:12:46 +1100

    commit 2

--- src/js/class/Character.js
+++ src/js/class/Character.js
@@ -3,10 +3,12 @@ export default class Character {
     constructor(name, type) {
         let defaultTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
-        if (typeof name !== 'string' || name.length < 2 || name.length > 10 || !defaultTypes.includes(type)) {
-            throw new Error('Некорректные данные');    
+        if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+            throw new Error('Некорректное имя');    
         }  
-   
+        if (!defaultTypes.includes(type)) {
+            throw new Error('Некорректный тип');    
+        }  
         this.name = name;
         this.type = type;
         this.health  = 100;
